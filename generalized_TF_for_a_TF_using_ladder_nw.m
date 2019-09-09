dfe4ce6863bbac5bfbc531675c173ada5c60d6e9
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2019-09-09 21:50:29 +0530

    code now generates the transfer function(s)

--- generalized_TF_for_a_TF_using_ladder_nw.m
+++ generalized_TF_for_a_TF_using_ladder_nw.m
@@ -4,14 +4,17 @@
     %1.1 Number of RLC ladders (physically speaking, coils in the TF)
     n=4;
     %1.2.1 Partial discharge in the form of a current source Is
-    %1.2.2 (to be defined later) Is=
+    %1.2.2 (to be defined later) Is=?
     %1.3.1 Location of node of partial discharge development (i):
     %1.3.2 Right now only ALONG the winding
     i=3;
-%Defining ladder nw constants:
+%***********************************************************************    
+%2.0 Defining ladder nw constants:
 Rs=1.33;Cs=0.6;Cg=0.933;Ls=0.4310;
-M=[0.2392,0.1435,0.0947,0.0612,0.0496,zeros(1,n-6)];    
-%Inductance (L) Matrix
+M=[0.2392,0.1435,0.0947,0.0612,0.0496,zeros(1,n-6)];
+%***********************************************************************    
+%3.0 Constructing matrices useful for state space representation:
+%3.1 Inductance (L) Matrix
 L=zeros(n,n);
 for r= 1:n
     for c= 1:n
@@ -22,22 +25,23 @@ for r= 1:n
         end
     end
 end
-%Resistance (R) Matrix
+%3.2 Resistance (R) Matrix
 R=Rs*eye(n);
-%(S,SL1,SL2,SN1,SN2) Matrices
-%L derived from when Live (node) shorted (to GND)
-%N derived from when Neutral (node) shorted (to GND)
+%3.3.1 (S,SL1,SL2,SN1,SN2) Matrices
+%3.3.2 L derived from when Live (node) shorted (to GND)
+%3.3.3 N derived from when Neutral (node) shorted (to GND)
 S=[-eye(n),zeros(n,1)]+[zeros(n,1),eye(n)];
 SL1=S;SL1(:,1)=[];
 SL2=zeros(n,1);SL2(i-1,:)=1;
 SN1=S;SN1(:,end)=[];
 SN2=zeros(n,1);SN2(i,:)=1;
-%Capacitance (C) Matrix with spinoffs (CL1,CN1)
+%3.4 Capacitance (C) Matrix with spinoffs (CL1,CN1)
 tmp=eye(n+1);tmp(1,1)=1/2;tmp(end,end)=1/2;
 C=(Cg+2*Cs)*tmp+(-Cs)*[zeros(n,1),eye(n);zeros(1,n+1)]+(-Cs)*[zeros(1,n+1);eye(n),zeros(n,1)];
 CL1=C;CL1(1,:)=[];CL1(:,1)=[];
 CN1=C;CN1(end,:)=[];CN1(:,end)=[];
-%Define (A),(B),(C),(D) matrices:
+%***********************************************************************    
+%4.0 Define (A),(B),(C),(D) matrices (State Space Representation):
     %But first, define (M),(-G),(T1):
     ML=[L,zeros(n,n);zeros(n,n),CL1];
     GL=-[-R,-SL1;SL1.',zeros(n,n)];
@@ -49,3 +53,37 @@ AL=-inv(ML)*GL;
 BL=inv(ML)*T1L;
 AN=-inv(MN)*GN;
 BN=inv(MN)*T1N;
+tmpL=zeros(1,2*n);tmpL(:,1)=1;
+tmpN=zeros(1,2*n);tmpN(:,n)=1;
+CL=[-Cs*AL(n+1,:)+tmpL];
+DL=[-Cs*BL(n+1,:)];
+CN=[Cs*AN(2*n,:)+tmpN];
+DN=[Cs*BN(2*n,:)];
+%*********************************************************************** 
+%5.0 Convert State Space Representation (SSR) into Transfer Function (TF)
+[TFLb,TFLa]=ss2tf(AL,BL,CL,DL);
+sysL=tf(TFLb,TFLa)
+[TFNb,TFNa]=ss2tf(AN,BN,CN,DN);
+sysN=tf(TFNb,TFNa)
+%***********************************************************************
+%6.0 Plot obtained TF responses, etc.
+subplot(4,2,1);
+step(sysL);
+subplot(4,2,2);
+impulse(sysL);
+subplot(4,2,3);
+bode(sysL);
+subplot(4,2,4);
+nyquist(sysL);
+grid;
+
+subplot(4,2,5);
+step(sysN);
+subplot(4,2,6);
+impulse(sysN);
+subplot(4,2,7);
+bode(sysN);
+subplot(4,2,8);
+nyquist(sysN);
+grid;
+%***********************************************************************    
